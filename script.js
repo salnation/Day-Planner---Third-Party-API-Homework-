328ca75c4b00ffa3b62913fa88ff5e60d4e9b11b
Stefan Salituro <stefansalituro@gmail.com>
2020-06-25 19:30:14 -0400

    Thursday evening commit. Some psuedocoding and some thoughts written out for my script.js. Also linked the jquery and the ajax <script> to the index.html.

---
+++ script.js
@@ -0,0 +1,7 @@
+// We have to create a function that displays date and time at the top of the page.
+// We must create a button with event listener to save to local storage 
+//When this button is clicked, this will trigger the function that stores input in local storage
+// Must create a loop 
+// Must add events to the submit button to save what is in the input field to local storage
+// Store data in localstorage as an object where the key is the hour of the day 
+// Create an array 
