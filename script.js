873be2222f89e45c40f4efcef682469355ef8452
Stefan Salituro <stefansalituro@gmail.com>
2020-06-25 22:09:09 -0400

    Final commit - script.js has been updated three var changed to let currentTime and const hour and const input - the day planner is ready.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // We have to create a function that displays date and time at the top of the page.
 // First Step - create a variable that displays current time using moment().format - information gathered from http://zetcode.com/javascript/momentjs/
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(currentTime);
 
 // We must create a function that performs the task of retreiving the current day and current time
@@ -20,20 +20,17 @@ $(document).ready(function() {
 // We must create a button with event listener to save to local storage 
 $(".save-button").on("click", function(){
     // attr() method - returns attributes and values of the selected parent element
-    var hour = $(this).parent().attr("id")
+    const hour = $(this).parent().attr("id")
     console.log(hour)
     // $(this) wrapping around that element
     // .val method in javascript referenced from https://www.w3schools.com/jquery/html_val.asp
     // Create reference for every .task class and change the innerHTML
-    var input = $(this).parent().siblings(".task").val();
+    const input = $(this).parent().siblings(".task").val();
     console.log(input)
     // localStorage.setItem is being utilized - the hour-9 value from localStorage should go in the appropriate input field
     localStorage.setItem(hour, input);
   })
 
 });
-// When this button is clicked, this will trigger the function that stores input in local storage
-// Must create a loop 
-// Must add events to the submit button to save what is in the input field to local storage
-// Store data in localstorage as an object where the key is the hour of the day 
-// Create an array 
+
+// Application is complete!
