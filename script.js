11ebc34492b3b43816970af741e40f873557f5cb
Stefan Salituro <stefansalituro@gmail.com>
2020-06-25 21:37:21 -0400

    8th thursday commit - almost forgot to link my javacscript file script.js- I couldn't figure out why my javascript wasn't working.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // We have to create a function that displays date and time at the top of the page.
 // First Step - create a variable that displays current time using moment().format - information gathered from http://zetcode.com/javascript/momentjs/
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(currentTime);
 
 // We must create a function that performs the task of retreiving the current day and current time
@@ -8,12 +8,15 @@ console.log(currentTime);
 function displayTime(){
     document.getElementById("currentDay").innerHTML = currentTime;
 };
+
 // calls a function or evaluates an expression at specified intervals - 1000 milliseconds
 setInterval(displayTime, 1000);
 displayTime()
 
 // $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
-$(document).ready(function()
+// A $( document ).ready() block
+$(document).ready(function() {
+
 // We must create a button with event listener to save to local storage 
 $(".save-button").on("click", function(){
     // attr() method - returns attributes and values of the selected parent element
@@ -22,11 +25,13 @@ $(".save-button").on("click", function(){
     // $(this) wrapping around that element
     // .val method in javascript referenced from https://www.w3schools.com/jquery/html_val.asp
     // Create reference for every .task class and change the innerHTML
-    var userInput = $(this).parent().siblings(".task").val();
-    console.log(userInput)
+    var input = $(this).parent().siblings(".task").val();
+    console.log(input)
     // localStorage.setItem is being utilized - the hour-9 value from localStorage should go in the appropriate input field
     localStorage.setItem(hour, input);
-})
+  })
+
+});
 // When this button is clicked, this will trigger the function that stores input in local storage
 // Must create a loop 
 // Must add events to the submit button to save what is in the input field to local storage
