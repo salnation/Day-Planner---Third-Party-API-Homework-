d7175cd591d70c2345ffb71a7be9a5371d286c6f
Stefan Salituro <stefansalituro@gmail.com>
2020-06-25 20:52:02 -0400

    4th thursday commit - created two variables - var hour and var userInput - when this button is clicked, this will trigger the function that stores input in local storage.

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ console.log(currentTime);
 // We must create a function that performs the task of retreiving the current day and current time
 
 function displayTime(){
-    document.getElementById("currentTime").innerHTML = currentTime;
+    document.getElementById("currentDay").innerHTML = currentTime;
 };
-// We must create a button with event listener to save to local storage 
 
-//When this button is clicked, this will trigger the function that stores input in local storage
+// We must create a button with event listener to save to local storage 
+$(".save-button").on("click", function(){
+    // attr() method - returns attributes and values of the selected parent element
+    var hour = $(this).parent().attr("id")
+    console.log(hour)
+    // $(this) wrapping around that element
+    // .val method in javascript referenced from https://www.w3schools.com/jquery/html_val.asp
+    // Create reference for every .task class and change the innerHTML
+    var userInput = $(this).parent().siblings(".task").val();
+    console.log(userInput)
+}
+// When this button is clicked, this will trigger the function that stores input in local storage
 // Must create a loop 
 // Must add events to the submit button to save what is in the input field to local storage
 // Store data in localstorage as an object where the key is the hour of the day 
 // Create an array 
-// Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed
